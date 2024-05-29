32cdea6af26334e9e7404129c8c684d979d27fb8
Anthony Christidis <anthony-alexander_christidis@hms.harvard.edu>
2024-05-29 01:13:19 -0400

    Allow detectAnomaly to only have a reference dataset.

--- R/detectAnomaly.R
+++ R/detectAnomaly.R
@@ -11,26 +11,22 @@
 #' PCA data, and optional PCA anomaly plots.
 #'
 #' @details
-#' This function first gets common genes between the reference and query data, and then applies log normalization to 
-#' the expression data. 
-#' It selects highly variable genes from the reference data and extracts the expression data and labels.
-#' The function then applies PCA to the entire reference expression data and predicts PCA scores for the query data. 
-#' It builds isolation forests and performs diagnostics for each cell type, calculating anomaly scores for the query data.
-#' The function also provides optional analysis of anomaly scores and creates a list of output for each cell type, 
-#' including PCA anomaly plots if `store_plots` is set to TRUE. 
-#' Finally, it returns a list containing the results for each cell type.
+#' This function first applies PCA to the entire reference expression data and predicts PCA scores for the query data if it is provided. 
+#' It builds isolation forests and performs diagnostics for each cell type, calculating anomaly scores for the query data (or reference 
+#' data if the query data is not provided).
 #' Isolation Forest is an algorithm for anomaly detection that works by building an ensemble of isolation trees. It is based on the 
 #' idea that anomalies are more susceptible to isolation than normal instances.
 #' The part where we project the query data onto the PCA space of the reference data is done by using the `predict` function on the PCA model with the query expression data. This allows us to transform the query data into the same PCA space as the reference data, which is necessary for the isolation forest analysis.
 #' 
-#' @param query_data A \code{\linkS4class{SingleCellExperiment}} object containing numeric expression matrix for the query cells.
-#' @param reference_data A \code{\linkS4class{SingleCellExperiment}} object containing numeric expression matrix for the reference cells.
+#' @param reference_data A \code{\linkS4class{SingleCellExperiment}} object containing numeric expression matrix for the 
+#' reference cells.
+#' @param query_data (Optional) A \code{\linkS4class{SingleCellExperiment}} object containing numeric expression matrix for the query cells. 
+#' If NULL, then the isolation forest anomaly scores are computed for the reference data. Default is NULL.
 #' @param query_cell_type_col A character string specifying the column name in the query dataset containing cell type annotations.
 #' @param ref_cell_type_col A character string specifying the column name in the reference dataset containing cell type annotations.
 #' @param n_components An integer specifying the number of principal components to use. Default is 10.
 #' @param n_tree An integer specifying the number of trees for the isolation forest. Default is 500
 #' @param anomaly_treshold A numeric value specifying the threshold for identifying anomalies, Default is 0.5.
-#' @param store_plots A logical value indicating whether to store PCA anomaly plots. Default is TRUE.
 #' @param ... Additional arguments passed to the `isolation.forest` function.
 #' 
 #' @return A list containing the results for each cell type, including anomaly scores, anomaly IDs and PCA data.
@@ -81,32 +77,42 @@
 #' ref_data_subset <- runPCA(ref_data_subset)
 #' 
 #' # Store PCA anomaly data and plots
-#' anomaly_output <- detectAnomaly(query_data_subset, ref_data_subset, 
+#' anomaly_output <- detectAnomaly(ref_data_subset, query_data_subset,
 #'                                 ref_cell_type_col = "reclustered.broad", 
 #'                                 query_cell_type_col = "labels",
 #'                                 n_components = 10,
 #'                                 n_tree = 500,
-#'                                 anomaly_treshold = 0.5,
-#'                                 store_plots = TRUE) 
+#'                                 anomaly_treshold = 0.5) 
 #' 
 #' # Plot the output for a cell type
 #' plot(anomaly_output, cell_type = "CD8", pc_subset = c(1:5))
 #' 
 # Function to perform diagnostics using isolation forest with PCA and visualization
-detectAnomaly <- function(query_data, reference_data, 
+detectAnomaly <- function(reference_data, 
+                          query_data = NULL, 
                           query_cell_type_col, 
                           ref_cell_type_col, 
                           n_components = 10,
                           n_tree = 500,
                           anomaly_treshold = 0.5,
-                          store_plots = TRUE,
                           ...) {
-  
-  # Get PCA data from reference and query datasets (query data projected onto PCA space of reference dataset)
-  pca_output <- projectPCA(query_data = query_data, reference_data = reference_data, 
-                           n_components = n_components, return_value = "list")
-  reference_pca <- pca_output$ref[, paste0("PC", 1:n_components)]
-  query_pca <- pca_output$query[, paste0("PC", 1:n_components)]
+    
+    # Check whether the anlaysis is done only for one dataset
+  if (is.null(query_data)) {
+      query_data <- reference_data
+      query_cell_type_col <- ref_cell_type_col
+  }
+    
+  if(!is.null(n_components)){
+      # Get PCA data from reference and query datasets (query data projected onto PCA space of reference dataset)
+      pca_output <- projectPCA(query_data = query_data, reference_data = reference_data, 
+                               n_components = n_components, return_value = "list")
+      reference_mat <- pca_output$ref[, paste0("PC", 1:n_components)]
+      query_mat <- pca_output$query[, paste0("PC", 1:n_components)]
+  } else{
+      reference_mat <- t(as.matrix(assay(reference_data, "logcounts")))
+      query_mat <- t(as.matrix(assay(query_data, "logcounts")))
+  }
   
   # List to store output
   output <- list()
@@ -116,12 +122,12 @@ detectAnomaly <- function(query_data, reference_data,
   query_labels <- query_data[[query_cell_type_col]]
   
   # Build isolation forests and perform diagnostics for each cell type
-  cell_types <- BiocGenerics::unique(query_labels)[!is.na(BiocGenerics::unique(query_labels))]  # Extract unique (query) cell types
+  cell_types <- na.omit(intersect(unique(reference_labels), unique(query_labels)))
   for (cell_type in cell_types) {
     
     # Filter reference and query PCA data for the current cell type
-    reference_pca_subset <- na.omit(reference_pca[reference_labels == cell_type,])
-    query_pca_subset <- na.omit(query_pca[query_labels == cell_type,])
+    reference_pca_subset <- na.omit(reference_mat[reference_labels == cell_type,])
+    query_pca_subset <- na.omit(query_mat[query_labels == cell_type,])
     
     # Build isolation forest on reference PCA data for this cell type
     isolation_forest <- isotree::isolation.forest(reference_pca_subset, ntree = n_tree, ...)
