47a914bd19c8db2ce9ca1f1386be241aae489995
Anthony Christidis <anthony.christidis@stat.ubc.ca>
2024-09-20 15:25:15 -0400

    Final fixes for Bioconductor.

--- R/calculateHotellingPValue.R
+++ R/calculateHotellingPValue.R
@@ -1,19 +1,19 @@
 #' @title Perform Hotelling's T-squared Test on PCA Scores for Single-cell RNA-seq Data
 #'
-#' @description 
+#' @description
 #' Computes Hotelling's T-squared test statistic and p-values for each specified cell type
 #' based on PCA-projected data from query and reference datasets.
 #'
-#' @details 
+#' @details
 #' This function calculates Hotelling's T-squared statistic for comparing multivariate means
 #' between reference and query datasets, projected onto a subset of principal components (PCs).
 #' It performs a permutation test to obtain p-values for each cell type specified.
 #'
 #' @param query_data A \code{\linkS4class{SingleCellExperiment}} object containing numeric expression matrix for the query cells.
 #' @param reference_data A \code{\linkS4class{SingleCellExperiment}} object containing numeric expression matrix for the reference cells.
-#' @param query_cell_type_col character. The column name in the \code{colData} of \code{query_data} 
+#' @param query_cell_type_col character. The column name in the \code{colData} of \code{query_data}
 #' that identifies the cell types.
-#' @param ref_cell_type_col character. The column name in the \code{colData} of \code{reference_data} 
+#' @param ref_cell_type_col character. The column name in the \code{colData} of \code{reference_data}
 #' that identifies the cell types.
 #' @param cell_types A character vector specifying the cell types to include in the plot. If NULL, all cell types are included.
 #' @param pc_subset A numeric vector specifying which principal components to include in the plot. Default is PC1 to PC5.
@@ -21,10 +21,10 @@
 #' @param assay_name Name of the assay on which to perform computations. Default is "logcounts".
 #'
 #' @return A named numeric vector of p-values from Hotelling's T-squared test for each cell type.
-#' 
+#'
 #' @references
 #' Hotelling, H. (1931). "The generalization of Student's ratio". *Annals of Mathematical Statistics*. 2 (3): 360–378. doi:10.1214/aoms/1177732979.
-#' 
+#'
 #' @export
 #'
 #' @author Anthony Christidis, \email{anthony-alexander_christidis@hms.harvard.edu}
@@ -33,26 +33,26 @@
 #' # Load data
 #' data("reference_data")
 #' data("query_data")
-#' 
+#'
 #' # Get the p-values
-#' p_values <- calculateHotellingPValue(query_data = query_data, 
-#'                                      reference_data = reference_data, 
-#'                                      query_cell_type_col = "SingleR_annotation", 
+#' p_values <- calculateHotellingPValue(query_data = query_data,
+#'                                      reference_data = reference_data,
+#'                                      query_cell_type_col = "SingleR_annotation",
 #'                                      ref_cell_type_col = "expert_annotation",
-#'                                      pc_subset = 1:10) 
+#'                                      pc_subset = 1:10)
 #' round(p_values, 5)
-#'                          
+#'
 # Function to perform Hotelling T^2 test for each cell type
 # The test is performed on the PCA space of the reference data The query data projected onto PCA space of reference
-calculateHotellingPValue <- function(query_data, 
-                                     reference_data, 
-                                     query_cell_type_col, 
+calculateHotellingPValue <- function(query_data,
+                                     reference_data,
+                                     query_cell_type_col,
                                      ref_cell_type_col,
                                      cell_types = NULL,
                                      pc_subset = 1:5,
                                      n_permutation = 500,
                                      assay_name = "logcounts") {
-    
+
     # Check standard input arguments
     argumentCheck(query_data = query_data,
                   reference_data = reference_data,
@@ -61,41 +61,43 @@ calculateHotellingPValue <- function(query_data,
                   cell_types = cell_types,
                   pc_subset_ref = pc_subset,
                   assay_name = assay_name)
-    
+
     # Get common cell types if they are not specified by user
     if(is.null(cell_types)){
         cell_types <- na.omit(unique(c(reference_data[[ref_cell_type_col]],
                                        query_data[[query_cell_type_col]])))
     }
-    
+
     # Get the projected PCA data
-    pca_output <- projectPCA(query_data = query_data, 
-                             reference_data = reference_data, 
-                             pc_subset = pc_subset, 
-                             query_cell_type_col = query_cell_type_col, 
+    pca_output <- projectPCA(query_data = query_data,
+                             reference_data = reference_data,
+                             pc_subset = pc_subset,
+                             query_cell_type_col = query_cell_type_col,
                              ref_cell_type_col = ref_cell_type_col,
                              assay_name = assay_name)
     cell_list <- split(pca_output, pca_output[["cell_type"]])
-    
+
     # Perform permutation test with p-values
     p_values <- numeric(length(cell_types))
     names(p_values) <- cell_types
     for (cell_type in cell_types) {
-        
+
         ref_ind <- cell_list[[cell_type]][["dataset"]] == "Reference"
-        observed_t2 <- hotellingT2(as.matrix(cell_list[[cell_type]][ref_ind, paste0("PC", pc_subset)]),
-                                   as.matrix(cell_list[[cell_type]][!ref_ind, paste0("PC", pc_subset)]))
-        
+        observed_t2_data <- as.matrix(cell_list[[cell_type]][, paste0("PC", pc_subset)])
+        observed_t2 <- hotellingT2(observed_t2_data[ref_ind,],
+                                   observed_t2_data[!ref_ind,])
+
         perm_t2 <- numeric(n_permutation)
         for(perm_id in seq_len(n_permutation)){
-            
+
             ref_sample_id <- sample(seq_len(nrow(cell_list[[cell_type]])), sum(ref_ind), replace = FALSE)
-            perm_t2[perm_id] <- hotellingT2(as.matrix(cell_list[[cell_type]][ref_sample_id, paste0("PC", pc_subset)]),
-                                            as.matrix(cell_list[[cell_type]][-ref_sample_id, paste0("PC", pc_subset)]))
+            perm_t2_data <- as.matrix(cell_list[[cell_type]][, paste0("PC", pc_subset)])
+            perm_t2[perm_id] <- hotellingT2(perm_t2_data[ref_sample_id,],
+                                            perm_t2_data[-ref_sample_id,])
         }
         p_values[cell_type] <- mean(observed_t2 < perm_t2)
     }
-    
+
     # Return p-values
     return(p_values)
 }
@@ -105,40 +107,40 @@ calculateHotellingPValue <- function(query_data,
 #' @description
 #' Calculates the Hotelling's T^2 statistic for comparing means of multivariate data.
 #'
-#' @param sample1 A numeric matrix or data frame of multivariate observations for sample 1, where rows are observations and columns 
+#' @param sample1 A numeric matrix or data frame of multivariate observations for sample 1, where rows are observations and columns
 #' are variables.
 #' @param sample2 A numeric matrix or data frame of multivariate observations for sample 2, with the same structure as sample 1.
-#' 
+#'
 #' @keywords internal
-#' 
+#'
 #' @return The Hotelling's T^2 statistic.
 #'
 # Function to calculate hotelling T2 statistic between two groups
 hotellingT2 <- function(sample1, sample2) {
-    
+
     # Number of observations in each sample
     n1 <- nrow(sample1)
     n2 <- nrow(sample2)
-    
+
     # Number of variables (columns) in each sample
     p <- ncol(sample1)
-    
+
     # Compute mean vectors for each sample
     mean1 <- colMeans(sample1)
     mean2 <- colMeans(sample2)
-    
+
     # Compute covariance matrices for each sample
     cov1 <- cov(sample1)
     cov2 <- cov(sample2)
-    
+
     # Pooled covariance matrix
     pooled_cov <- ((n1 - 1) * cov1 + (n2 - 1) * cov2) / (n1 + n2 - 2)
-    
+
     # Compute Hotelling's T^2 statistic
     # T^2 = n1 * n2 / (n1 + n2) * (mean1 - mean2)' * pooled_cov^-1 * (mean1 - mean2)
-    t2 <- n1 * n2 / (n1 + n2) * t(mean1 - mean2) %*% solve(pooled_cov) %*% 
+    t2 <- n1 * n2 / (n1 + n2) * t(mean1 - mean2) %*% solve(pooled_cov) %*%
         (mean1 - mean2)
-    
+
     # Return the computed T^2 statistic
     return(as.numeric(t2))
 }
--- R/plotGeneSetScores.R
+++ R/plotGeneSetScores.R
@@ -1,85 +1,81 @@
 #' @title Visualization of gene sets or pathway scores on dimensional reduction plot
-#' 
+#'
 #' @description
 #' Plot gene sets or pathway scores on PCA, TSNE, or UMAP. Single cells are color-coded by scores of gene sets or pathways.
-#' 
-#' @details 
-#' This function plots gene set scores on reduced dimensions such as PCA, t-SNE, or UMAP. 
+#'
+#' @details
+#' This function plots gene set scores on reduced dimensions such as PCA, t-SNE, or UMAP.
 #' It extracts the reduced dimensions from the provided SingleCellExperiment object.
 #' Gene set scores are visualized as a scatter plot with colors indicating the scores.
-#' For PCA, the function automatically includes the percentage of variance explained 
+#' For PCA, the function automatically includes the percentage of variance explained
 #' in the plot's legend.
-#'          
+#'
 #' @param se_object An object of class \code{\linkS4class{SingleCellExperiment}} containing numeric expression matrix and other metadata.
 #'        It can be either a reference or query dataset.
 #' @param method A character string indicating the method for visualization ("PCA", "TSNE", or "UMAP").
 #' @param score_col A character string representing the name of the score_col (score) in the colData(se_object) to plot.
-#' @param pc_subset An optional vector specifying the principal components (PCs) to include in the plot if method = "PCA". 
+#' @param pc_subset An optional vector specifying the principal components (PCs) to include in the plot if method = "PCA".
 #'        Default is 1:5.
 #'
 #' @return A ggplot2 object representing the gene set scores plotted on the specified reduced dimensions.
-#' 
+#'
 #' @export
-#' 
+#'
 #' @author Anthony Christidis, \email{anthony-alexander_christidis@hms.harvard.edu}
 #'
 #' @examples
 #' # Load data
 #' data("query_data")
-#' 
+#'
 #' # Plot gene set scores on PCA
-#' plotGeneSetScores(se_object = query_data, 
-#'                   method = "PCA", 
+#' plotGeneSetScores(se_object = query_data,
+#'                   method = "PCA",
 #'                   score_col = "gene_set_scores",
 #'                   pc_subset = 1:5)
-#'                   
-#' # Note: Users can provide their own gene set scores in the colData of the 'se_object' object, 
+#'
+#' # Note: Users can provide their own gene set scores in the colData of the 'se_object' object,
 #' # using any dimension reduction of their choice.
 #'
 # Function to plot gene get scores in PCA space
-plotGeneSetScores <- function(se_object, 
-                              method = c("PCA", "TSNE", "UMAP"), 
+plotGeneSetScores <- function(se_object,
+                              method = c("PCA", "TSNE", "UMAP"),
                               score_col,
                               pc_subset = 1:5) {
-    
+
     # Match method argument
     method <- match.arg(method)
-    
+
     # Check if dimention reduction method is present in reference's reducedDims
     if (!(method %in% names(reducedDims(se_object)))) {
         stop("\'se_object\' must have pre-computed \'", method, "\' in \'reducedDims\'.")
     }
-    
+
     # Check standard input arguments
     argumentCheck(query_data = se_object,
                   pc_subset_query = pc_subset)
-    
+
     # Check if score_col is a valid column name in se_object
     if (!score_col %in% colnames(colData(se_object))) {
         stop("score_col: '", score_col, "' is not a valid column name in se_object.")
     }
-    
+
     # Create the plot object
     if (method == "PCA") {
-        
+
         # PCA data
         plot_mat <- reducedDim(se_object, "PCA")[, pc_subset]
         # Modify column names to include percentage of variance explained
         colnames(plot_mat) <- paste0(
-            "PC", pc_subset, 
-            " (", sprintf("%.1f%%", 
+            "PC", pc_subset,
+            " (", sprintf("%.1f%%",
                           attributes(reducedDim(se_object, "PCA"))[["percentVar"]][pc_subset]), ")")
-    } else if (method == "TSNE") {
-        
-        # TSNE data
-        plot_mat <- reducedDim(se_object, "TSNE")
-        
-    } else if (method == "UMAP") {
-        
-        # UMAP data
-        plot_mat <- reducedDim(se_object, "UMAP")
+    } else {
+
+        # TSNE or UMAP data
+        plot_mat <- reducedDim(se_object, method)
+
     }
-    
+
     # Create all possible pairs of specified PCs
     plot_names <- colnames(plot_mat)
     pairs <- expand.grid(x = plot_names, y = plot_names)
@@ -97,31 +93,31 @@ plotGeneSetScores <- function(se_object,
     # Plot data
     data_pairs <- do.call(rbind, data_pairs_list)
     # Remove redundant data (to avoid duplicated plots)
-    data_pairs <- data_pairs[as.numeric(data_pairs[["x"]]) < 
+    data_pairs <- data_pairs[as.numeric(data_pairs[["x"]]) <
                                  as.numeric(data_pairs[["y"]]),]
     data_pairs[["Scores"]] <- se_object[[score_col]]
     # Create the ggplot object (with facets if PCA)
     plot_obj <- ggplot2::ggplot(
-        data_pairs, ggplot2::aes(x = .data[["x_value"]], 
-                                 y = .data[["y_value"]], 
+        data_pairs, ggplot2::aes(x = .data[["x_value"]],
+                                 y = .data[["y_value"]],
                                  color = .data[["Scores"]])) +
-        ggplot2::geom_point(size = 1, alpha = 0.5) + 
-        ggplot2::xlab("") + ggplot2::ylab("") + 
+        ggplot2::geom_point(size = 1, alpha = 0.5) +
+        ggplot2::xlab("") + ggplot2::ylab("") +
         ggplot2::scale_color_gradientn(
-            colors = c("#2171B5", "#8AABC1", "#FFEDA0", "#E6550D"), 
-            values = seq(0, 1, by = 1/3), 
+            colors = c("#2171B5", "#8AABC1", "#FFEDA0", "#E6550D"),
+            values = seq(0, 1, by = 1/3),
             limits = c(0, max(data_pairs$Scores))) +
         ggplot2::facet_grid(
-            rows = ggplot2::vars(.data[["y"]]), 
+            rows = ggplot2::vars(.data[["y"]]),
             cols = ggplot2::vars(.data[["x"]]), scales = "free") +
         ggplot2::theme_bw() +
         ggplot2::theme(
             panel.grid.minor = ggplot2::element_blank(),
-            panel.grid.major = ggplot2::element_line(color = "gray", 
+            panel.grid.major = ggplot2::element_line(color = "gray",
                                                      linetype = "dotted"),
-            plot.title = ggplot2::element_text(size = 14, face = "bold", 
+            plot.title = ggplot2::element_text(size = 14, face = "bold",
                                                hjust = 0.5),
-            axis.title = ggplot2::element_text(size = 12), 
+            axis.title = ggplot2::element_text(size = 12),
             axis.text = ggplot2::element_text(size = 10))
     return(plot_obj)
 }
